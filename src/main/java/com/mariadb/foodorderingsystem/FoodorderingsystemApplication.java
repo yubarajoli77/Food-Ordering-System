d2507b500076a3218e5822f65f8cf9ad458985b9
yubaraj <yubarajoli77@gmail.com>
2017-09-04 10:31:08 +0545

    first setup

---
+++ src/main/java/com/mariadb/foodorderingsystem/FoodorderingsystemApplication.java
@@ -0,0 +1,12 @@
+package com.mariadb.foodorderingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodorderingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodorderingsystemApplication.class, args);
+	}
+}
